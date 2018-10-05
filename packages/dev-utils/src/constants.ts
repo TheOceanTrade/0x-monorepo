2916f91efc6a51b9ffe1806cdaa59f0806effa47
Ari Kardasis <ari.kardasis@gmail.com>
2018-10-05 15:12:43 -0400

    updated constants file for custom dev

--- packages/dev-utils/src/constants.ts
+++ packages/dev-utils/src/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
-    RPC_URL: 'http://localhost:8545',
+    RPC_URL: 'http://localchain:8545',
     RPC_PORT: 8545,
-    GAS_LIMIT: 7000000,
-    TESTRPC_FIRST_ADDRESS: '0x5409ed021d9299bf6814279a6a1411a7e866a631',
+    GAS_LIMIT: 5000000,
+    TESTRPC_FIRST_ADDRESS: '0x00a329c0648769a73afac7f9381e08fb43dbea72',
 };
