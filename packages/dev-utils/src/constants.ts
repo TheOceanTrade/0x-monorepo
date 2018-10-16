2859ea823d975f6b969a61829e303870bd72ea32
Ari Kardasis <ari.kardasis@gmail.com>
2018-10-16 09:44:04 -0400

    changed gas limit back to 7,000,000

--- packages/dev-utils/src/constants.ts
+++ packages/dev-utils/src/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
     RPC_URL: 'http://localchain:8545',
     RPC_PORT: 8545,
-    GAS_LIMIT: 100000000,
+    GAS_LIMIT: 7000000,
     TESTRPC_FIRST_ADDRESS: '0xd2a58c877927a0b01966f0f08579c9181f25ead9',
 };
